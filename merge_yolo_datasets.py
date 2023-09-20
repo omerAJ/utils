a8b3f706784c18ed38ca95510cb86d522e5abb98
omerAJ <112098102+omerAJ@users.noreply.github.com>
2023-09-20 19:21:00 +0500

    Update merge_yolo_datasets.py
    dealt with conflicting file names

--- merge_yolo_datasets.py
+++ merge_yolo_datasets.py
@@ -16,11 +16,22 @@ def merge_yaml_files(yaml_files):
                     merged_classes.append(name)
     return all_classes, {'names': merged_classes}
 
-def merge_folders(src_folder, dest_folder, class_map):
+def merge_folders(src_folder, dest_folder, class_map, existing_files):
     for filename in os.listdir(src_folder):
         src_path = os.path.join(src_folder, filename)
+        
+        # Handle filename collisions
+        original_filename = filename
+        counter = 1
+        while filename in existing_files:
+            name, ext = os.path.splitext(original_filename)
+            filename = f"{name}_{counter}{ext}"
+            counter += 1
+        existing_files.add(filename)
+        
         dest_path = os.path.join(dest_folder, filename)
-        if filename.endswith(".txt"):
+        
+        if original_filename.endswith(".txt"):
             with open(src_path, 'r') as f:
                 lines = f.readlines()
             new_lines = []
@@ -47,6 +58,9 @@ def main(root_dirs, output_dir):
     
     with open(os.path.join(output_dir, 'data.yaml'), 'w') as f:
         yaml.dump(merged_yaml_data, f)
+        
+    existing_train_files = set()
+    existing_test_files = set()
 
     for i, root_dir in enumerate(root_dirs):
         with open(os.path.join(root_dir, 'data.yaml'), 'r') as f:
@@ -57,14 +71,15 @@ def main(root_dirs, output_dir):
         for idx, name in enumerate(names):
             class_map[idx] = class_map_list[name]
         
-        for data_type in ['train', 'test']:
+        for data_type in ['train', 'test', 'val']:
+            existing_files = existing_train_files if data_type == 'train' else existing_test_files
             src_label_folder = os.path.join(root_dir, data_type, 'labels')
             dest_label_folder = os.path.join(output_dir, data_type, 'labels')
-            merge_folders(src_label_folder, dest_label_folder, class_map)
+            merge_folders(src_label_folder, dest_label_folder, class_map, existing_files)
 
             src_image_folder = os.path.join(root_dir, data_type, 'images')
             dest_image_folder = os.path.join(output_dir, data_type, 'images')
-            merge_folders(src_image_folder, dest_image_folder, class_map)
+            merge_folders(src_image_folder, dest_image_folder, class_map, existing_files)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Merge multiple YOLO datasets into one.")
